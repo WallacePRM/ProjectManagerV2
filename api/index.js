c6a94860a7963222930bd1837ac85be9a4a3adfe
Wallace <57970046+INextzBGD@users.noreply.github.com>
2021-06-22 21:17:05 -0300

    tls config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 
 (async () => {
 
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
     const routes = require('./routes');
     const { executeMigration } = require('./database/index');
     
