a7e4f2328a55786899e987a39224d4c76c23d850
Walisson Pires Rodrigues Miranda <walissonpiresgv@hotmail.com>
2022-02-20 11:11:02 -0300

    [Feature] Shared projects between teams

--- api/database/scripts/migration-20210517.sql
+++ api/database/scripts/migration-20210517.sql
@@ -1,39 +1,65 @@
 
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY,
 	email VARCHAR(50) NOT NULL,
 	password VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE projects (
-	id SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(100),
     estimated_time VARCHAR(10),
 	price FLOAT,
 
 	user_id INTEGER NOT NULL,
-	
+
 	FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE tasks (
-	id SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
-	
+
     project_id INTEGER NOT NULL,
 	user_id INTEGER NOT NULL,
-	
+
 	FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
 );
 
 CREATE TABLE history (
-	id SERIAL PRIMARY KEY, 
+	id SERIAL PRIMARY KEY,
 	action VARCHAR(5) NOT NULL,
 	date TIMESTAMP NOT NULL,
 
 	task_id INTEGER NOT NULL,
 
-	FOREIGN KEY (task_id) REFERENCES tasks(id)
+	FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
+);
+
+CREATE TABLE teams (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	user_id INTEGER NOT NULL,
+
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE team_members (
+	team_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+
+	PRIMARY KEY (team_id, user_id),
+	FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE team_projects (
+	team_id INTEGER NOT NULL,
+	project_id INTEGER NOT NULL,
+
+	PRIMARY KEY (team_id, project_id),
+	FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
+	FOREIGN KEY (project_id) REFERENCES projects(id)
 );
